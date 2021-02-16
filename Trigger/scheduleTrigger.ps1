b11ea98742cb3286327fb71323e3f784660c2d92
Sarit Maitra <sarit.maitra@gmail.com>
2021-02-16 18:19:15 +0800

    Create scheduleTrigger.ps1

---
+++ Trigger/scheduleTrigger.ps1
@@ -0,0 +1,11 @@
+#create trigger
+Set-AzDataFactoryV2Trigger -ResourceGroupName $ResourceGroupName -DataFactoryName $DataFactoryName -Name "MyTrigger" -DefinitionFile "C:\ADFv2QuickStartPSH\MyTrigger.json"
+
+#Start the trigger 
+Start-AzDataFactoryV2Trigger -ResourceGroupName $ResourceGroupName -DataFactoryName $DataFactoryName -Name "MyTrigger"
+
+#status of trigger
+Get-AzDataFactoryV2Trigger -ResourceGroupName $ResourceGroupName -DataFactoryName $DataFactoryName -Name "MyTrigger"
+
+#information aboit trigger run
+Get-AzDataFactoryV2TriggerRun -ResourceGroupName $ResourceGroupName -DataFactoryName $DataFactoryName -TriggerName "MyTrigger" -TriggerRunStartedAfter "2017-12-08T00:00:00" -TriggerRunStartedBefore "2017-12-08T01:00:00"
